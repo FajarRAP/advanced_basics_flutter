d9a41b1325d7051f28494341acaba0fac8c2165e
FajarRAP <fajary781@gmail.com>
2023-08-05 17:24:27 +0700

    membuat model pertanyaan dan pertanyaannya

---
+++ lib/data/data_soal.dart
@@ -0,0 +1,22 @@
+import 'package:advanced_basics/models/pertanyaan_kuis.dart';
+
+const List<PertanyaanKuis> pertanyaan = [
+  PertanyaanKuis("Merek HP Ainx apa?", [
+    "Poco",
+    "Samsung",
+    "Xiaomi",
+    "Rexus",
+  ]),
+  PertanyaanKuis("Tanggal lahir ainx kapan?", [
+    "19 November 2002",
+    "21 Februari 1996",
+    "8 September 1967",
+    "21 Desember 1971",
+  ]),
+  PertanyaanKuis("Compfest diadakan oleh?", [
+    "Universitas Indonesia",
+    "Universitas Ahmad Dahlan",
+    "Pakuwon Mall",
+    "Izanagi"
+  ]),
+];
---
+++ lib/models/pertanyaan_kuis.dart
@@ -0,0 +1,6 @@
+class PertanyaanKuis {
+  const PertanyaanKuis(this.soal, this.opsiJawaban);
+
+  final String soal;
+  final List<String> opsiJawaban;
+}
